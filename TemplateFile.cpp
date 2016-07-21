9d1bf122e06e8fb0536aa702f34b001106d6fde9
Jellyshen <gdshen95@gmail.com>
2016-07-21 18:42:43 +0800

    Initial commit

---
+++ TemplateFile.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Guodong Shen on 7/21/16.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "hello, world";
+   return 0;
+}
+
---
+++ chapt2/violent_search/recursive.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Guodong Shen on 7/21/16.
+//
+
